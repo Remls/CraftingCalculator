a3cd20053a8011ebe7d1fe643bd71c952ef0d65f
Adam Raaif Nasheed <adam.raaif.nasheed7@gmail.com>
2023-05-31 14:21:43 +0500

    Add simplified preset calculations
    Closes #1

--- docs/assets/scripts.js
+++ docs/assets/scripts.js
@@ -1,3 +1,5 @@
+const KEYS = ['five', 'four', 'three', 'two', 'one']
+
 document.addEventListener('alpine:init', () => {
     Alpine.data('craftData', function () {
         return {
@@ -15,6 +17,14 @@ document.addEventListener('alpine:init', () => {
                 two: 0,
                 one: 0,
             }),
+            isDisabled: this.$persist({
+                five: false,
+                four: false,
+                three: false,
+                two: false,
+                one: false,
+            }),
+
             haveSimplified: 0,
             needSimplified: 0,
             diffSimplified: 0,
@@ -42,23 +52,26 @@ document.addEventListener('alpine:init', () => {
                 }
             },
             simplify(inp) {
-                this.validate(inp)
-                let five = inp.five ?? 0
-                let four = inp.four ?? 0
-                let three = inp.three ?? 0
-                let two = inp.two ?? 0
-                let one = inp.one ?? 0
-                return (five * 81) + (four * 27) + (three * 9) + (two * 3) + (one * 1)
+                inp = this.validate(inp)
+                let power = 0
+                let sum = 0
+                KEYS.slice().reverse().forEach(key => {
+                    if (this.isDisabled[key]) return
+                    sum += inp[key] * Math.pow(3, power)
+                    power++
+                })
+                return sum
             },
             validate(inp) {
-                for (let key in inp) {
+                KEYS.forEach(key => {
                     if (!inp[key]) {
                         inp[key] = 0
                     }
                     if (inp[key] <= 0) {
                         inp[key] = 0
                     }
-                }
+                })
+                return inp
             },
             clearValueIfZero($e) {
                 if ($e.target.value == 0) {
@@ -66,7 +79,19 @@ document.addEventListener('alpine:init', () => {
                 }
             },
             loadPreset(five, four, three, two, one) {
+                this.isDisabled = {
+                    five: five == 0,
+                    four: four == 0,
+                    three: three == 0,
+                    two: two == 0,
+                    one: one == 0,
+                }
                 this.need = { five, four, three, two, one }
+                KEYS.forEach(key => {
+                    if (this.isDisabled[key]) {
+                        this.have[key] = 0
+                    }
+                })
                 this.updateCalculations()
             },
             reset(type) {
@@ -83,6 +108,13 @@ document.addEventListener('alpine:init', () => {
                 localStorage.clear()
                 this.reset('have')
                 this.reset('need')
+                this.isDisabled = {
+                    five: false,
+                    four: false,
+                    three: false,
+                    two: false,
+                    one: false,
+                }
             }
         }
     })
