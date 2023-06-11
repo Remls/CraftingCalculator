850250aa8b7df9aecec9ed52ab3d5ff65aa16b65
Adam Raaif Nasheed <adam.raaif.nasheed7@gmail.com>
2023-06-11 14:44:04 +0500

    Calculations now take highest need rarity into account
    Closes #8

--- src/components/ProgressDisplay.svelte
+++ src/components/ProgressDisplay.svelte
@@ -2,14 +2,47 @@
   import { tweened } from "svelte/motion";
   import { cubicOut } from "svelte/easing";
   import { have, need, isDisabled } from "../stores/base";
-  import { type Craftable } from "../types";
+  import { type Craftable, type Rarity } from "../types";
+  import { keyNames, keyNamesReversed, keys } from "../helpers/constants";
 
-  const simplify = (input: Craftable) => {
+  $: highestNeed = (() => {
+    for (let i = 0; i < keyNames.length; i++) {
+      if ($isDisabled[keyNames[i]]) continue;
+      if ($need[keyNames[i]] > 0) {
+        return {
+          rarity: keyNames[i],
+          limit: $need[keyNames[i]],
+        };
+      }
+    }
+    return {
+      rarity: "one" as Rarity,
+      limit: 0,
+    };
+  })();
+
+  const simplify = (input: Craftable, limitHighestRarity = false) => {
     let power = 0;
     let sum = 0;
-    ["one", "two", "three", "four", "five"].forEach((key) => {
+    keyNamesReversed.forEach((key) => {
       if ($isDisabled[key]) return;
-      sum += input[key] * Math.pow(3, power);
+      if (limitHighestRarity) {
+        const current = keyNamesReversed.indexOf(key);
+        const highest = keyNamesReversed.indexOf(highestNeed.rarity);
+        if (current > highest) {
+          // None of this rarity can be used
+          sum += 0;
+        } else if (current == highest) {
+          // This rarity can be used, only up to the limit
+          sum += Math.min(input[key], highestNeed.limit) * Math.pow(3, power);
+        } else {
+          // This rarity can be used, all of it
+          sum += input[key] * Math.pow(3, power);
+        }
+      } else {
+        // No limit
+        sum += input[key] * Math.pow(3, power);
+      }
       power++;
     });
     return sum;
@@ -20,26 +53,34 @@
     easing: cubicOut,
   });
 
-  $: haveSimplified = simplify($have);
-  $: needSimplified = simplify($need);
-  $: diff = (() => {
-    if (needSimplified == 0) return 0;
-    if (needSimplified < haveSimplified) return 0;
-    return needSimplified - haveSimplified;
+  $: simplifiedValues = (() => {
+    const haveSm = simplify($have, true);
+    const needSm = simplify($need);
+    let diff = needSm - haveSm;
+    if (diff < 0) diff = 0;
+    return {
+      have: haveSm,
+      need: needSm,
+      diff,
+    };
   })();
-  $: if (needSimplified == 0) {
+  $: if (simplifiedValues.need == 0) {
     progressValue.set(0);
   } else {
-    progressValue.set((haveSimplified / needSimplified) * 100);
+    progressValue.set((simplifiedValues.have / simplifiedValues.need) * 100);
   }
 </script>
 
 <div>
-  <progress max="100" value={$progressValue} class:full={$progressValue >= 100} />
+  <progress
+    max="100"
+    value={$progressValue}
+    class:full={$progressValue >= 100}
+  />
   <br />
-  {haveSimplified} / {needSimplified}
-  {#if diff > 0}
-    ({diff} remaining)
+  {simplifiedValues.have} / {simplifiedValues.need}
+  {#if simplifiedValues.diff > 0}
+    ({simplifiedValues.diff} remaining)
   {/if}
   <br />
   {$progressValue.toFixed(2)}%
